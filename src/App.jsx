e8b624d5953bbc5f0a89a7e5e1ad8f2398bb2f28
Abood <abdelhakim.merzoug00@gmail.com>
2022-02-24 23:37:12 +0100

    Added main components and style loader.

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import './style.css';
 
 const App = () => {
     return (
-        <>
+        <div >
             <Navbar />
             <Home />
             <Footer />
-        </>
+        </div>
     );
 };
 
